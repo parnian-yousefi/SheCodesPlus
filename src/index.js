ca96cafc334abbc7ccb9a20375cd68d494ad25e2
Parnian Yousefi Sabet <p_yousefi_s@yahoo.com>
2023-03-26 18:36:12 +0430

    Added the Forecast Functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 let apiKey = "597c40c39084687093b091cd48b366f8";
+let forecastKey = "bc2cd97eaa209e7d22d8f3c84081655f";
 
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Tehran&appid=${apiKey}&&units=metric`;
+let forecastUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=0&lon=0&appid=${forecastKey}`;
+
+let currentDate = new Date();
+let today = document.querySelector("#date");
+let weekDays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let weekDaysShort = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 function findPosition(position) {
   let latitude = position.coords.latitude;
@@ -23,17 +39,58 @@ function displayWeather(response) {
   let city = document.querySelector("h1");
   let windSpeed = document.querySelector("#wind");
   let condition = document.querySelector("#condition");
-  let icon = document.querySelector("#conImg");
+  let icon = document.querySelector("#weatherIcon");
+  let humidity = document.querySelector("#humidity");
+  let max = document.querySelector("#dayMax");
+  let min = document.querySelector("#dayMin");
   city.innerHTML = response.data.name;
   tempreture.innerHTML = Math.round(currentTemp);
   console.log(response.data);
   windSpeed.innerHTML = `Wind Speed: ${response.data.wind.speed} km/h`;
   condition.innerHTML = `${response.data.weather[0].description}`;
-  icon.src = `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
+  humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  icon.innerHTML = `<img
+            src="images/${response.data.weather[0].icon}.svg"
+            alt="Semi Cloudy"
+            class="weather"
+            id="conImg"
+          />`;
+  max.innerHTML = `${Math.round(response.data.main.temp_max)}`;
+  min.innerHTML = `${Math.round(response.data.main.temp_min)}`;
+  forecastUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${response.data.coord.lat}&lon=${response.data.coord.lon}&appid=${forecastKey}&units=metric`;
+  axios.get(forecastUrl).then(forecastWeather);
+}
+
+function forecastWeather(response) {
+  console.log(response.data);
+  let week = document.querySelector(".week");
+  console.log(currentDate.getDay());
+  let weekDay = currentDate.getDay() + 1;
+  week.innerHTML = "";
+  for (let i = 0, j = currentDate.getDay() + 1; i < 6; i++, j++) {
+    week.innerHTML =
+      week.innerHTML +
+      `<div class="col-2">
+          <p class="weekDay">${
+            weekDaysShort[(currentDate.getDay() + j) % 7]
+          }</p>
+          <img
+            src="images/${response.data.daily[i + 1].weather[0].icon}.svg"
+            alt="${response.data.daily[i + 1].weather[0].main}"
+            class="weather"
+          />
+          <p class="temp">
+            <span class="max">${Math.round(
+              response.data.daily[i + 1].temp.max
+            )}</span>° / <span class="min">${Math.round(
+        response.data.daily[i + 1].temp.min
+      )}</span>°
+          </p>
+        </div>`;
+  }
 }
 
 navigator.geolocation.getCurrentPosition(findPosition);
-axios.get(apiUrl).then(displayWeather);
 
 function toFahrenheit(cels) {
   return (cels * 9) / 5 + 32;
@@ -91,23 +148,9 @@ function changeToCelsius() {
   document.querySelector("#fahrenheit").disabled = false;
 }
 
-let currentDate = new Date();
-let today = document.querySelector("#date");
-let weekDays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-
-let weekDaysShort = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
 function getWeekDays() {
   let days = document.querySelectorAll(".weekDay");
-  let i = 1;
+  let i = currentDate.getDay() + 1;
   days.forEach((element) => {
     element.innerHTML = weekDaysShort[(currentDate.getDay() + i) % 7];
     i++;
