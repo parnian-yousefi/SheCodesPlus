19c30f2da5a12e18d5d7f3443414c33287a814c5
Parnian Yousefi Sabet <p_yousefi_s@yahoo.com>
2023-03-26 21:54:26 +0430

    Update index.js

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ function displayWeather(response) {
   console.log(response.data);
   windSpeed.innerHTML = `Wind Speed: ${response.data.wind.speed} km/h`;
   condition.innerHTML = `${response.data.weather[0].description}`;
-  icon.src = `../images/cloudy.svg`;
+  icon.src = `https://github.com/parnian-yousefi/SheCodesPlus/tree/main/images/01d.svg`;
 }
 
 navigator.geolocation.getCurrentPosition(findPosition);
